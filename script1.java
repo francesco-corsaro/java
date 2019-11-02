47155753dc149098b5a2bc9e5ed7fad5a27f72d0
francesco-corsaro <57091974+francesco-corsaro@users.noreply.github.com>
2019-11-02 22:53:09 +0100

    Add files via upload

---
+++ script1.java
@@ -0,0 +1,9 @@
+public class script1{
+	public mioMetodo(){
+	System.out.println("Ciao belli!);
+} /*metodo è la stessa cosa di dire funzione in php
+	In questo ho creato la funzione che scrive Ciao belli*/
+public static void main (String[]arg){
+	miometodo();
+} /*adesso ho dato il comando di eseguire il metodo che ho sctitto sopra*/
+}	
