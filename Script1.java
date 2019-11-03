c4dc9dc709ec28dd5a2b0b10c9ee45d143a8e1ea
francesco-corsaro <57091974+francesco-corsaro@users.noreply.github.com>
2019-11-03 18:40:18 +0100

    Update and rename script1.java to Script1.java

--- Script1.java
+++ Script1.java
@@ -1,9 +1,9 @@
-public class script1{
+public class Script1{
 	public mioMetodo(){
-	System.out.println("Ciao belli!);
+	System.out.println("Ciao belli!");
 } /*metodo è la stessa cosa di dire funzione in php
 	In questo ho creato la funzione che scrive Ciao belli*/
-public static void main (String[]arg){
-	miometodo();
+public static void main (String[]args){
+	mioMetodo();
 } /*adesso ho dato il comando di eseguire il metodo che ho sctitto sopra*/
 }	
