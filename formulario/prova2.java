bd28f04947a3a78d20da33ccd80e0727cfa2ac59
francesco-corsaro <57091974+francesco-corsaro@users.noreply.github.com>
2019-11-07 19:36:29 +0100

    Delete prova2.java

--- formulario/prova2.java
+++
@@ -1,7 +0,0 @@
-// Subclass (inherit from Animal)
-class Pig extends Animal {
-  public void animalSound() {
-    // The body of animalSound() is provided here
-    System.out.println("The pig says: wee wee");
-}
-}
